38520ccc4054cf60d3f4cb226e4c9319b8b8e149
Ethan Morchy <emorchy@gmail.com>
2021-02-05 23:46:58 -0800

    Add module metadata

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name="automation", packages=find_packages())
+setup(name="automation",
+      version='0.1.0',
+      description='Automatic ciphertext decoder'
+      long_description='Automate the identification and decoding of various ciphers',
+      author='Ethan Morchy',
+      author_email='emorchy@gmail.com',
+      packages=find_packages(),
+      python_requires='>=3.6')
