edf4079be7533558a7939ee8ab98d13fe4ee4897
Ethan Morchy <emorchy@gmail.com>
2021-02-07 00:05:50 -0800

    Commented out long_description

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name="automation",
       version='0.1.0',
-      description='Automatic ciphertext decoder'
-      long_description='Automate the identification and decoding of various ciphers',
+      description='Automate the identification and decoding of various ciphers',
       author='Ethan Morchy',
       author_email='emorchy@gmail.com',
       packages=find_packages(),
